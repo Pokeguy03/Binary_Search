fa2e190f87d9ae96d326a628b5ced0c72be4ef4b
Gurunath_M <142033469+Pokeguy03@users.noreply.github.com>
2026-01-08 20:02:54 +0530

    Create Binary_Search_Upper_Bound.py

---
+++ Binary_Search_Upper_Bound.py
@@ -0,0 +1,20 @@
+def lower(arr,target,n):
+    low=0
+    high=n-1
+    while low<=high:
+        mid=(high+low)//2
+        if arr[mid]>target:
+            ans=mid
+            high=mid-1
+        else:
+            low=mid+1 
+    return ans
+    
+arr=[1,2,4,5,6,7,7,8]
+target=6
+n=len(arr)
+sol=lower(arr,target,n)
+print(sol)
+
+#Formula:
+# arr[index]>target
