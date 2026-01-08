2d18d5519d3beb357e6a485e9fd12ab6445387b2
Gurunath_M <142033469+Pokeguy03@users.noreply.github.com>
2026-01-08 20:25:09 +0530

    Create Binary_Search_Floor_&_Ceil.py

---
+++ Binary_Search_Floor_&_Ceil.py
@@ -0,0 +1,35 @@
+#Ceil
+#nums[mid]>=target
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        low=0
+        high=n-1
+        ans=n
+        while low<=high:
+            mid=(high+low)//2
+            if nums[mid]>=target:
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1 
+        return ans
+      
+
+#Floor
+#nums[mid]<=target
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        low=0
+        high=n-1
+        ans=-1
+        while low<=high:
+            mid=(high+low)//2
+            if nums[mid]<=target:
+                ans=mid
+                low=mid+1
+            else:
+                high=mid-1 
+        return ans
+            
